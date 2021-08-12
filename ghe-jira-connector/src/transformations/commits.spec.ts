8146da10a7503c25a5f5728a55d7cf59f831dc7d
Karina Moraes da Silva <kmoraesdasilva@atlassian.com>
2021-08-12 15:17:00 +1000

    spec for unit tests

---
+++ ghe-jira-connector/src/transformations/commits.spec.ts
@@ -0,0 +1,13 @@
+import { buildRepository } from './commits';
+var assert = require('assert');
+import 'mocha';
+
+describe('Commits transformer', () => {
+
+    it('should return a repository with a commit', () => {
+
+       // const result = buildRepository(bla bla bla)
+       //  assert.equal(result, expected)
+    });
+
+});
